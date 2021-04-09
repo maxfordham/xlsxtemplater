6c2d4ed6708ba459fee2645facc8b8035fa04588
john <j.gunstone@maxfordham.com>
2021-04-09 16:09:28 +0100

    updated conda recipe to include python 3.9

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="high-level wrapper that sits on top of xlsxwriter,\
         to template the output of the pandas dataframes to formatted excel tables",
--- xlsxtemplater/__init__.py
+++ xlsxtemplater/__init__.py
@@ -1,3 +1,7 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+# not sure about this...
+from xlsxtemplater import xlsxtemplater
+from xlsxtemplater import templaterdefs
