1552ae7c7b5ef26e46e2340e8f0fa1d7d6ed9e59
gunstonej <gunstone.john@gmail.com>
2021-04-14 10:48:05 +0100

    simplified __init__

--- xlsxtemplater/__init__.py
+++ xlsxtemplater/__init__.py
@@ -1,7 +1,12 @@
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+
 
 # not sure about this...
-from xlsxtemplater import xlsxtemplater
-from xlsxtemplater import templaterdefs
+
+# see here:
+# https://stackoverflow.com/Questions/16981921/relative-imports-in-python-3
+# for discussion
+#import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__))) #  not sure if this works when executing a script from jupyter
+
+from _version import get_versions
+__version__ = get_versions()['version']
+del get_versions
