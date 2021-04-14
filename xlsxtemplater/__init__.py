f5970637a48810f7c29d613da1456178a74469ed
gunstonej <gunstone.john@gmail.com>
2021-04-14 16:50:11 +0100

    added simplified api imports

--- xlsxtemplater/__init__.py
+++ xlsxtemplater/__init__.py
@@ -7,6 +7,10 @@
 # for discussion
 #import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__))) #  not sure if this works when executing a script from jupyter
 
+#  add here for simplified api
+from templater import to_excel
+from utils import from_excel
+
 from _version import get_versions
 __version__ = get_versions()['version']
 del get_versions
